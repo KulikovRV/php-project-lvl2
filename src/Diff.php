a7cddff5bb8c1e2a3690c65a84b7dbf642ef5840
Roman Kulikov <texasultra@mail.ru>
2021-06-15 12:11:07 +0300

    fix: save diff changes

--- src/Diff.php
+++ src/Diff.php
@@ -2,15 +2,58 @@
 
 namespace Differ\Differ;
 
-function genDiff($pathToFile1, $pathToFile2)
+// - есть в первом файле, в во втором нет,
+// - одинаковые ключи, разные значения
+// + появился во втором файле, уникальные данные
+// + измененные данные
+// отсортированы по алфавиту
+// как реализовать плюс и минус ?
+// на выходе json ?
+// пока работаем с плоскими массивоми
+
+// {
+//  - follow: false
+//    host: hexlet.io
+//  - proxy: 123.234.53.22
+//  - timeout: 50
+//  + timeout: 20
+//  + verbose: true
+//}
+
+
+use function Functional\if_else;
+
+function genDiff($pathToFile1, $pathToFile2): array
 {
-    $j1 = file_get_contents($pathToFile1, true);
-    $j2 = file_get_contents($pathToFile2, true);
-    print_r($j1);
-    print_r($j2);
+    $file1 = json_decode(file_get_contents($pathToFile1, true), true);
+    $file2 = json_decode(file_get_contents($pathToFile2, true), true);
+    $diff = [];
+    foreach ($file1 as $key1 => $item1) {
+        foreach ($file2 as $key2 => $item2) {
+            if ($key1 === $key2 && $item1 === $item2) {
+                $diff[$key1] = $item1;
+            } elseif ($key1 === $key2 && $item1 !== $item2) {
+                $newKey1 = "- $key1";
+                $newKey2 = "+ $key2";
+                $diff[$newKey1] = $item1;
+                $diff[$newKey2] = $item2;
+            } elseif ($key1 !== $key2) {
+                $newKey3 = "- $key1";
+                $diff[$newKey3] = $item1;
+            }
+
+        }
+    }
+    return $diff;
+
+//    return array_map(function ($item1, $item2) {
+//        if ($item1 === $item2) {
+//            return $item1;
+//        }
+//    }, $file1, $file2);
 }
 
 $pathToFile1 = 'file1.json';
 $pathToFile2 = 'file2.json';
 
-genDiff($pathToFile1, $pathToFile2);
+print_r(genDiff($pathToFile1, $pathToFile2));
