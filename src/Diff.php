fb14cc4b02feeb8d70ae91fb2a9c1c4f20acb97e
Roman Kulikov <texasultra@mail.ru>
2021-11-03 11:06:19 +0300

    fix Diff.php, testing new iter2

--- src/Diff.php
+++ src/Diff.php
@@ -4,10 +4,11 @@ namespace App\Differ;
 
 function genDiff($file1, $file2)
 {
-    return iter($file1, $file2);
+
+    return iter2($file1, $file2);
 }
 
-function iter($array1, $array2, $diff = [])
+function iter1($array1, $array2, $diff = [])
 {
     if (!is_array($array1)) {
         return $array1;
@@ -52,6 +53,56 @@ function iter($array1, $array2, $diff = [])
 
     ksort($diff);
     //var_dump($diff);
+    return $diff;
+}
+
+function iter2($array1, $array2, $diff = [])
+{
+    if (!is_array($array1)) {
+        return $array1;
+        /*return [
+            'status' => '...',
+            'value' => $array1
+        ];*/
+    }
+
+    if (!is_array($array2)) {
+        return $array2;
+        /*return [
+            'status' => '...',
+            'value' => $array2
+        ];*/
+    }
+
+    $array1Keys = array_keys($array1);
+    $array2Keys = array_keys($array2);
+    $uniqueKeys = array_intersect_key($array1Keys, $array2Keys);
+    ksort($uniqueKeys);
+    var_dump($uniqueKeys);
+
+    foreach ($uniqueKeys as $key) {
+        if (array_key_exists($key, $array1) && array_key_exists($key, $array2)) {
+            $diff[$key] = [
+                'status' => 'saved',
+                'value' => iter2($array1[$key], $array2[$key], $diff)
+            ];
+        }
+
+        if (!array_key_exists($key, $array1) && array_key_exists($key, $array2)) {
+            $diff[$key] = [
+                'status' => 'new',
+                'value' => $array2[$key]
+            ];
+        }
+
+        if (array_key_exists($key, $array1) && !array_key_exists($key, $array2)) {
+            $diff[$key] = [
+                'status' => 'deleted',
+                'value' => $array1[$key]
+            ];
+        }
+    }
+
     return $diff;
 }
 
