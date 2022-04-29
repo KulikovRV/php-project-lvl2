95a24406200f45b47f58ce9d938d748bf0038495
Roman Kulikov <texasultra@mail.ru>
2022-04-29 13:59:08 +0300

    add: new check value

--- src/Parser.php
+++ src/Parser.php
@@ -2,12 +2,18 @@
 
 namespace App\Parser;
 
+use Exception;
 use Symfony\Component\Yaml\Yaml;
 
 function preparationOfFiles(string $pathToFile)
 {
     $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
     $content = file_get_contents($pathToFile, true);
+
+    if ($content === false) {
+        throw new Exception("Cannot access '$pathToFile' to read contents.");
+    }
+
     $data = '';
 
     return match ($extension) {
